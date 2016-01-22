43cb0a3d7de9a3ef32db6aba8f8b2150eb7547bc
niek <niek.pot@gmail.com>
2016-01-22 14:34:15 +0100

    changes to function made for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Frankly, I just followed the example function and changed mean to inverse
 
-## Write a short comment describing this function
+##when you put in a matrix x, the function: 
+##sets the value of the matrix, 
+##gets the value of the matrix, 
+##sets the value of the inverse 
+##gets the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse_mat <- NULL
+  set <- function (y){
+    x <<- y
+    inverse_mat <<- NULL
+  }
+  get <-function() x
+  setInverse <- function(inverse) inverse_mat <<- inverse
+  getInverse <- function() inverse_mat
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Again, I used the example as a blue print
+## this function:
+## checks if inverse is already set
+## if not, calculates the inverse and sets is as inverse_mat
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse_mat <- x$getInverse()
+  if(!is.null(inverse_mat)){
+    message("get inverse from cache")
+    return(inverse_mat)
+  }
+  data <- x$get()
+  inverse_mat <- solve(data, ...)
+  x$setInverse(inverse_mat)
+  inverse_mat
 }
